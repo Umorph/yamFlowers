90be9f27cbac7390c2081380f3023e4547cdb76a
Vladislav Kantariya <umorphine@gmail.com>
2020-10-14 22:06:17 +0400

    Add configuratior on slider block

--- #src/js/slickSettings.js
+++ #src/js/slickSettings.js
@@ -2,4 +2,9 @@ $(document).ready(function(){
     $('.offer-slider').slick({
         dots: true,
     });
+
+    $('.slider-offer__slider').slick({
+        autoplay: true,
+        autoplaySpeed: 1500,
+    });
 });
